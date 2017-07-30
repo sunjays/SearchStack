ae2938a14c84c43da17d591e5ae016aec7dba367
Sunjay <sunjays@live.com>
2017-07-30 17:07:08 +0530

    Create Card.js

---
+++ src/components/Card.js
@@ -0,0 +1,65 @@
+import React,{Component} from 'react';
+
+import QuestionCard from './QuestionCard';
+
+import FullCard from './FullCard';
+
+import './Card.css'
+
+class Card extends Component{
+
+    constructor(props){
+
+        super(props);
+
+        this.state = {
+
+            display : "question"
+
+        };
+
+        this.handleCardClick = this.handleCardClick.bind(this);
+
+    }
+
+    handleCardClick(answer_count) {
+
+        //console.log(answer_count);
+
+        this.setState(prevState => (
+
+            prevState.display === "question" ? {
+
+                display : "answer",
+
+                } : {
+
+                display : "question",
+
+            }))
+
+    }
+
+    render(){
+
+        return(
+
+                <div className="card">
+
+                {    this.state.display === 'question' ?
+
+                    <QuestionCard handleCardClick={this.handleCardClick} {...this.props.item}/> :
+
+                    <FullCard handleCardClick={this.handleCardClick} {...this.props.item}/>}
+
+                </div>
+
+        )
+
+    }
+
+}
+
+
+
+export default Card;
